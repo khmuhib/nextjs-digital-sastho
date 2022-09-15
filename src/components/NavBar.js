1ef8ffa75fac8766f3dadc6a42fc07a452b77592
kh muhib <khmuhib2013@gmail.com>
2022-09-16 02:23:19 +0600

    menu test

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,14 +7,14 @@ export default function NavBar() {
       <div className="row bg-slate-500">
         <div className="container mx-auto bg-black">
           <ul className='flex'>
-            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3'><a href="">Home</a></li>
-            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3'><a href="">About</a></li>
-            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3 relative group'><a href="">Service</a>
+            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3'><a href="#">Home</a></li>
+            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3'><a href="#">About</a></li>
+            <li className='bg-yellow-50 hover:bg-red-200 px-3 py-3 relative group'><a href="#">Service</a>
               <ul className='bg-yellow-400 absolute top-full left-0 hidden group-hover:block'>
-                <li><a href="">Sub 01</a></li>
-                <li><a href="">Sub 01</a></li>
-                <li><a href="">Sub 01</a></li>
-                <li><a href="">Sub 01</a></li>
+                <li><a href="#">Sub 01</a></li>
+                <li><a href="#">Sub 01</a></li>
+                <li><a href="#">Sub 01</a></li>
+                <li><a href="#">Sub 01</a></li>
               </ul>
             
             </li>
