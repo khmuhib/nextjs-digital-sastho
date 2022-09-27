8cbcc9731d58651971cfa25b95d0d783ed835ffd
khmuhib <khmuhib2013@gmail.com>
2022-09-27 15:04:00 +0600

    ...

--- components/HomeOurTopFeature.js
+++ components/HomeOurTopFeature.js
@@ -6,9 +6,9 @@ export default function HomeOurTopFeature() {
         <>
             <div className="row py-[100px]">
                 <div className="container mx-auto">
-                    <div className="grid md:grid-cols-3 grid-cols-1 gap-10 ">
-                        <div className="relative before:content-[''] before:absolute before:block md:before:h-[300px] md:before:w-[600px] before:border-2 before:border-[#2BB673] before:rounded-tr-[150px] before:rounded-br-[150px] md:before:top-[-30px] md:before:left-[150px] z-[-10] before:top-[-30px] before:left-[100px] before:h-[300px] before:w-[320px] px-3">
-                            <div className="bg-[#128041] md:h-full h-[405px] w-[300px] md:m-auto flex flex-col justify-center items-center rounded-md z-10 relative ml-5">
+                    <div className="grid md:grid-cols-4 grid-cols-1 md:gap-x-16 gap-10">
+                        <div className="relative before:content-[''] before:absolute before:block md:before:h-[300px] md:before:w-[600px] before:border-2 before:border-[#2BB673] before:rounded-tr-[150px] before:rounded-br-[150px] md:before:top-[-30px] md:before:left-[150px] z-[-10] before:top-[-30px] before:left-[100px] before:h-[300px] before:w-[320px] px-3 md:col-span-2 lg:col-span-1">
+                            <div className="bg-[#128041] md:h-full md:w-full w-[300px] h-[405px] flex flex-col justify-center items-center rounded-md z-10 relative ml-5">
                                 <div className="">
                                     <h2 className='table text-[38px] font-bold text-white before:block before:h-[5px] before:w-[50px] before:bg-white before:absolute before:left-[80px] before:top-[32px]'>Our</h2>
                                     <h2 className='text-[38px] font-bold text-white'>Top Features</h2>
@@ -16,8 +16,8 @@ export default function HomeOurTopFeature() {
                                 </div>
                             </div>
                         </div>
-                        <div className="md:col-span-2">
-                            <div className="bg-[#F9F9F9] w-[85%] py-10 md:px-10 rounded-md m-auto shadow">
+                        <div className="md:col-span-2 lg:col-span-3">
+                            <div className="bg-[#F9F9F9] w-[85%] py-10 md:px-10 rounded-md shadow m-auto md:m-0">
                                 <div className="flex flex-col gap-10">
                                     <div className="flex gap-5 items-center px-5">
                                         <div className="">
@@ -33,7 +33,7 @@ export default function HomeOurTopFeature() {
                                     <div className="flex gap-5 items-center px-5">
                                         <div className="">
                                             <div className="relative h-[50px] w-[50px]">
-                                                <Image src="/assets/images/f1.png" alt="logo" layout="fill" />
+                                                <Image src="/assets/images/f2.png" alt="logo" layout="fill" />
                                             </div>
                                         </div>
                                         <div className="">
@@ -44,7 +44,7 @@ export default function HomeOurTopFeature() {
                                     <div className="flex gap-5 items-center px-5">
                                         <div className="">
                                             <div className="relative h-[50px] w-[50px]">
-                                                <Image src="/assets/images/f1.png" alt="logo" layout="fill" />
+                                                <Image src="/assets/images/f3.png" alt="logo" layout="fill" />
                                             </div>
                                         </div>
                                         <div className="">
