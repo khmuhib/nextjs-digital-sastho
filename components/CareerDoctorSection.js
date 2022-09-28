6903d425cd1147af99d80688febb9d6431a003c1
khmuhib <khmuhib2013@gmail.com>
2022-09-28 18:00:39 +0600

    career page fixing

--- components/CareerDoctorSection.js
+++ components/CareerDoctorSection.js
@@ -7,11 +7,11 @@ export default function CareerDoctorSection() {
             <div className="row bg-[url('/assets/images/cr4.png')] py-[100px] bg-cover bg-no-repeat">
                 <div className="container mx-auto">
                     <div className="grid md:grid-cols-2 grid-cols-1 items-center gap-20 md:gap-0 px-5 md:px-0">
-                        <div className="overflow-hidden">
+                        <div className="">
                             <div className="">
-                                <div className="w-[400px] flex flex-col h-[350px] justify-center items-center m-auto relative rounded-md before:content-[''] before:block before:absolute before:h-[260px] before:w-[350px] before:border-2 before:border-white before:rounded-tl-[100px] before:rounded-br-[50px] before:bottom-[-40px] md:before:left-[-60px] before:left-[-40px]">
+                                <div className="md:w-[400px] md:h-[350px] w-[300px] h-[350px] flex flex-col justify-center items-center m-auto relative rounded-md before:content-[''] before:block before:absolute before:h-[260px] before:w-[350px] before:border-2 before:border-white before:rounded-tl-[100px] before:rounded-br-[50px] before:bottom-[-40px] md:before:left-[-60px] before:left-[-25px]">
 
-                                    <div className="bg-[url('/assets/images/hand.png')] bg-top-bottom bg-no-repeat relative h-[450px] w-full bg-red-300 before:content-[''] before:absolute before:block before:h-[50px] before:w-full before:bg-white before:bottom-0 before:right-0 before:z-10 before:rounded-tl-[40px]">
+                                    <div className="bg-[url('/assets/images/hand.png')] bg-top-bottom bg-no-repeat relative h-[450px] w-full bg-red-300 before:content-[''] before:absolute before:block before:h-[50px] before:w-full before:bg-white before:bottom-0 before:right-0 before:z-10 before:rounded-tl-[40px] overflow-hidden rounded-md">
                                         <h3 className='text-[#128041] absolute bottom-0 left-0 right-0 m-auto table py-4 z-10'>Verified Doctor</h3>
                                     </div>
                                 </div>
